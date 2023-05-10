75c93e7215569e5b763deb16722c3de66e68d3a5
Kai Weluda <kaiweluda@gmail.com>
2023-05-10 08:42:26 +0200

    feat: adds hex display

--- color-mixer/script.js
+++ color-mixer/script.js
@@ -4,7 +4,7 @@ Vue.createApp({
       red: 10,
       green: 150,
       blue: 150,
-      display: "Yolo",
+      hex: "#0A9696",
     };
   },
   methods: {
@@ -17,9 +17,15 @@ Vue.createApp({
     changeBlue(event) {
       this.blue = event.target.value;
     },
-    toHex(inputString) {
-      let output = "0" + parseInt(inputString).toString(16);
-      return output.slice(-2);
+    changeHex() {
+      function toHex(inputString) {
+        let output = "0" + parseInt(inputString).toString(16);
+        return output.slice(-2);
+      }
+      const output =
+        "#" + toHex(this.red) + toHex(this.green) + toHex(this.blue);
+
+      this.hex = output.toUpperCase();
     },
   },
   computed: {
@@ -30,67 +36,3 @@ Vue.createApp({
     },
   },
 }).mount("#app");
-
-// // selects the buttons and value display
-// const redButton = document.querySelector("#red");
-// const greenButton = document.querySelector("#green");
-// const blueButton = document.querySelector("#blue");
-
-// const valueDisplay = document.querySelector("#color-display");
-// const colorSliders = document.querySelector("#range-display");
-
-// // converts incoming strings to hex and puts a leading zero in front if needed
-// function toHex(inputString) {
-//   let output = "0" + parseInt(inputString).toString(16);
-//   return output.slice(-2);
-// }
-
-// // this function sets the background color based on the sliders state
-// function changeBackground() {
-//   document.body.style.setProperty("--red", redButton.value);
-//   document.body.style.setProperty("--green", greenButton.value);
-//   document.body.style.setProperty("--blue", blueButton.value);
-// }
-
-// // sets the initial background
-// changeBackground();
-
-// // displays the initial color value in hex
-// valueDisplay.textContent =
-//   "#" +
-//   toHex(redButton.value) +
-//   toHex(greenButton.value) +
-//   toHex(blueButton.value);
-
-// // event listener for the hex display
-// colorSliders.addEventListener("input", () => {
-//   valueDisplay.textContent =
-//     "#" +
-//     toHex(redButton.value) +
-//     toHex(greenButton.value) +
-//     toHex(blueButton.value);
-// });
-
-// // event listener for changes in the sliders
-// colorSliders.addEventListener("input", () => {
-//   changeBackground();
-// });
-
-/* LEGACY CODE AFTER THIS */
-
-// this sets the initial background color based on the sliders state
-// document.body.style.setProperty("--red", redButton.value);
-// document.body.style.setProperty("--green", greenButton.value);
-// document.body.style.setProperty("--blue", blueButton.value);
-
-// redButton.addEventListener("input", () => {
-//   document.body.style.setProperty("--red", redButton.value);
-// });
-
-// greenButton.addEventListener("input", () => {
-//   document.body.style.setProperty("--green", greenButton.value);
-// });
-
-// blueButton.addEventListener("input", () => {
-//   document.body.style.setProperty("--blue", blueButton.value);
-// });
