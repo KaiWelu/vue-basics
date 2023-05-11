fcbbceea5ea71b0de5a33b979c4cfb3929c3b1d4
Kai Weluda <kaiweluda@gmail.com>
2023-05-11 10:36:46 +0200

    feat: adds functioning password check

--- password-checker/script.js
+++ password-checker/script.js
@@ -3,10 +3,11 @@ Vue.createApp({
     return {
       passwordOne: "",
       passwordTwo: "",
-      isEqual: false,
+      equality: false,
       passwordHidden: true,
     };
   },
+
   computed: {
     inputType() {
       if (this.passwordHidden === true) {
@@ -22,10 +23,42 @@ Vue.createApp({
         return "Hide Password";
       }
     },
+    lowerCase() {
+      if (this.passwordOne.match(/[a-z]/) === null || this.equality === false) {
+        return false;
+      }
+      return true;
+    },
+    upperCase() {
+      if (this.passwordOne.match(/[A-Z]/) === null || this.equality === false) {
+        return false;
+      }
+      return true;
+    },
+    containsNumbers() {
+      if (this.passwordOne.match(/[0-9]/) === null || this.equality === false) {
+        return false;
+      }
+      return true;
+    },
+    passwordLength() {
+      if (this.passwordOne.length < 10 || this.equality === false) {
+        return false;
+      }
+      return true;
+    },
   },
+
   methods: {
     passwordToggle() {
       this.passwordHidden = !this.passwordHidden;
+    },
+    checkEqual() {
+      if (this.passwordOne === this.passwordTwo && this.passwordOne !== "") {
+        this.equality = true;
+      } else {
+        this.equality = false;
+      }
     },
   },
 }).mount("#app");
