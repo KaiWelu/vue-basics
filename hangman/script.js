9afe3d26c7f4ed80fc9b79454be9e6f79f6964f2
Kai Weluda <kaiweluda@gmail.com>
2023-05-11 15:43:00 +0200

    feat: adds win/fail checks, rendering of the guessword and some state management

--- hangman/script.js
+++ hangman/script.js
@@ -15,32 +15,43 @@ Vue.createApp({
     fails() {
       return this.badChars.length;
     },
-    isDisabled() {
-      if (this.buttons === false) {
-        return false;
-      } else {
-        return true;
-      }
-    },
   },
   methods: {
     buttonClick(event) {
       if (this.guessWord.includes(event.target.textContent) === true) {
         this.goodChars.push(event.target.textContent);
-        event.target.disabled = true;
+        // event.target.disabled = true;
       } else {
         this.badChars.push(event.target.textContent);
-        event.target.disabled = true;
+        // event.target.disabled = true;
       }
-      console.log(event.target.textContent);
       console.log(this.goodChars);
+      console.log(this.badChars);
     },
     restart() {
       this.gameState = "start";
       this.guessWord = "kai";
       this.goodChars = [];
       this.badChars = [];
-      this.buttons = false;
+    },
+    checkForWinOrFail() {
+      const word = new Set();
+      const guess = new Set();
+
+      this.guessWord.split("").forEach((char) => {
+        word.add(char);
+      });
+      this.goodChars.forEach((char) => {
+        guess.add(char);
+      });
+
+      if (word.size === guess.size) {
+        this.gameState = "success";
+        console.log("WIN!");
+      } else if (this.badChars.length === 10) {
+        this.gameState = "fail";
+        console.log("FAIL!");
+      }
     },
   },
 }).mount("#app");
